82dcf936dbfda1063f248df801328ecac7fa54b8
Tommy Parnell <tparnell8@gmail.com>
2015-11-10 19:04:08 -0500

    put a delay in so we dont 404 and figure out something better #4

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ azurestore.prototype.save = function (image) {
     var uniqueName = new Date().getMonth() +"/" + new Date().getFullYear()+"/"+ image.name;
     return nodefn.call(fileService.createContainerIfNotExists.bind(fileService), options.container, {publicAccessLevel: 'blob'})
     .then(nodefn.call(fileService.createBlockBlobFromLocalFile.bind(fileService), options.container, uniqueName, image.path))
+    .delay(500) //todo: this was placed here per issue #4 (aka sometimes things 404 right after upload) figure out a better way than just adding a delay
     .then(function(){
         return fileService.getUrl(options.container, uniqueName);
     });
